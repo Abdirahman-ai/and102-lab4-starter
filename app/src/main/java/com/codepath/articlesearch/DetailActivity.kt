2cb2701481cfbe4627e4e64c37492ff17d8aeeee
Abdirahman-ai <62496069+Abdirahman-ai@users.noreply.github.com>
2024-03-22 01:54:36 -0500

    finished lab04

--- app/src/main/java/com/codepath/articlesearch/DetailActivity.kt
+++ app/src/main/java/com/codepath/articlesearch/DetailActivity.kt
@@ -5,6 +5,8 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.bumptech.glide.GlideExperiments
+import kotlin.math.abs
 
 private const val TAG = "DetailActivity"
 
@@ -19,11 +21,23 @@ class DetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_detail)
 
         // TODO: Find the views for the screen
+        mediaImageView = findViewById(R.id.mediaImage)
+        titleTextView = findViewById(R.id.mediaTitle)
+        bylineTextView = findViewById(R.id.mediaByline)
+        abstractTextView = findViewById(R.id.mediaAbstract)
 
         // TODO: Get the extra from the Intent
+        val article = intent.getSerializableExtra(ARTICLE_EXTRA) as Article
 
         // TODO: Set the title, byline, and abstract information from the article
+        titleTextView.text = article.headline?.main
+        bylineTextView.text = article.byline?.original
+        abstractTextView.text = article.abstract
 
         // TODO: Load the media image
+
+        Glide.with(this)
+            .load(article.mediaImageUrl)
+            .into(mediaImageView)
     }
 }
--- app/src/main/java/com/codepath/articlesearch/MainActivity.kt
+++ app/src/main/java/com/codepath/articlesearch/MainActivity.kt
@@ -19,17 +19,19 @@ fun createJson() = Json {
     useAlternativeNames = false
 }
 
-private const val TAG = "MainActivity/"
-private const val SEARCH_API_KEY = BuildConfig.API_KEY
-private const val ARTICLE_SEARCH_URL =
+private val TAG = "MainActivity/"
+private val SEARCH_API_KEY = BuildConfig.API_KEY
+private val ARTICLE_SEARCH_URL =
     "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${SEARCH_API_KEY}"
 
 class MainActivity : AppCompatActivity() {
+    private val articles = mutableListOf<Article>()
     private lateinit var articlesRecyclerView: RecyclerView
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
@@ -37,6 +39,8 @@ class MainActivity : AppCompatActivity() {
 
         articlesRecyclerView = findViewById(R.id.articles)
         // TODO: Set up ArticleAdapter with articles
+        val articleAdapter = ArticleAdapter(this, articles)
+        articlesRecyclerView.adapter = articleAdapter
 
         articlesRecyclerView.layoutManager = LinearLayoutManager(this).also {
             val dividerItemDecoration = DividerItemDecoration(this, it.orientation)
@@ -58,10 +62,25 @@ class MainActivity : AppCompatActivity() {
                 Log.i(TAG, "Successfully fetched articles: $json")
                 try {
                     // TODO: Create the parsedJSON
+                    val parsedJson = createJson().decodeFromString(
+                        SearchNewsResponse.serializer(),
+                        json.jsonObject.toString()
+                    )
 
                     // TODO: Do something with the returned json (contains article information)
+                    // TODO: Save the articles
+                    parsedJson.response?.docs?.let { list ->
+                        articles.addAll(list)
+                    }
 
                     // TODO: Save the articles and reload the screen
+                    // Save the articles
+                    parsedJson.response?.docs?.let { list ->
+                        articles.addAll(list)
+
+                        // Reload the screen
+                        articleAdapter.notifyDataSetChanged()
+                    }
 
                 } catch (e: JSONException) {
                     Log.e(TAG, "Exception: $e")
